6c9efd92c808fec02b7bf362086f97c96b2e4be1
Tim Wissel <wissel.tim@gmail.com>
2020-03-15 15:44:55 +0100

    Call /items/getAll from frontend (needs to be added on REST API)

--- packages/server/public/api.js
+++ packages/server/public/api.js
@@ -1,11 +1,12 @@
 $.fn.api.settings.api = {
   'get participants': '/participant/getAll/{org}/{user}',
-  'register participant': '/participant/register'
+  'register participant': '/participant/register',
+  'get items': '/items/getAll/{org}/{user}',
 };
 
 $('#load-participants').api({
   action: 'get participants',
-  urlData: {
+  urlData: {//TODO remove code
     org: "SocialHousing",
     user: "Sultan"
   },
@@ -13,6 +14,34 @@ $('#load-participants').api({
     $('.list').empty();
     $.each(res, function (index, item) {
       $('.list').append(`<div class='item'><div class='content'>${item._name}</div></div>`);
+    });
+  }
+});
+
+$('#load-items').api({
+  action: 'get items',
+  urlData: {
+    org: "SocialHousing",
+    user: "Sultan"
+  },
+  onSuccess(res) {
+    $('.cards').empty();
+    $.each(res, function (index, item) {
+      $('.cards').append(`
+      <div class="card">
+            <div class="content">
+              <div class="header">${item._name}</div>
+              <div class="meta">${item._id}</div>
+              <div class="description">
+                <p>
+                Created: ${item._created}<br>
+                Description: <br>
+                Quality: ${item._quality}<br>
+                Owner: ${item._owner}<br>
+                </p>
+              </div>
+            </div>
+          </div>`);
     });
   }
 });
