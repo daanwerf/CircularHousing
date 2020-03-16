91024cafe1764abace905b48a0cdf5a61c3f7cc7
Tim Wissel <wissel.tim@gmail.com>
2020-03-16 12:51:04 +0100

    Add launchBlockchain to readme

--- launchBlockchain.sh
+++ launchBlockchain.sh
@@ -1,5 +1,10 @@
 hurl clean
+# Create an identities manager (currently only works for the first organisation
+# specified in network.config.json)
 hurl new -n ./network.config.json
 node ./packages/admin/registerIdManager.js Government
+# Package smart contract's code
 npm run cc:package -- participant
+# Install the chaincode to the blockchain (for debug mode, add --debug to the end of the command)
+# to the organizations defined in network.config.json file
 hurl install participant node -P ./chaincode-participant -o Government -o SocialHousing -o TableMaker -o WoodGatherer
