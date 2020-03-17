6a05ea63e0d49e00fa62e9a135a7e1c4659b41c6
Casper Athmer <casperathmer92@gmail.com>
2020-03-17 16:24:25 +0100

    Add script to get certificate of user and organisation

---
+++ packages/admin/get_certificate.js
@@ -0,0 +1,17 @@
+/*
+ * Register and Enroll a user
+ */
+
+// Get the organisation to register an admin for
+const org = process.argv[2]
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const x509 = require('x509');
+
+cert = JSON.parse(fs.readFileSync(path.resolve(os.homedir(), 
+    'hyperledger-fabric-network/.hfc-WoodGatherer/Casper'), 'utf8'))
+    .enrollment.identity.certificate;
+var parsed = x509.parseCert(cert);
+console.log(parsed.fingerPrint);
