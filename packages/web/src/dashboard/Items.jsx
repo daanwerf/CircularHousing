267686d1846274f4ee3bab506a11790878e92c27
Casper Athmer <casperathmer92@gmail.com>
2020-03-30 14:23:26 +0200

    Enable view of items per participant

--- packages/web/src/dashboard/Items.jsx
+++ packages/web/src/dashboard/Items.jsx
@@ -9,6 +9,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableItem from './TableItem';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Alert from '@material-ui/lab/Alert';
 import Title from './Title';
 import UpdateItem from './UpdateItem';
 
@@ -23,19 +24,31 @@ const useStyles = makeStyles(theme => ({
 
 export default function Items(props) {
   const [items, setItems] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
+  const loading = props.loading;
+  const setLoading = props.setLoading;
   const [update, setUpdate] = React.useState('');
   const [updateId, setUpdateId] = React.useState('');
+  const [alert, setAlert] = React.useState('');
+  const itemFetch = props.apiCall;
 
   const classes = useStyles();
 
   React.useEffect(() => {
     if (loading) {
-      fetch('http://localhost:8000/item/getAll?org=Government&user=chaincodeAdmin')
-        .then(results => results.json())
-        .then(data => {
-          setItems(data);
-          setLoading(false);
+      setAlert('');
+
+      fetch('http://localhost:8000/item/' + itemFetch + '?org=' + props.org + '&user=' + props.user)
+        .then(results => {
+          if (results.status === 200) {
+            results.json()
+            .then(data => {
+              setItems(data);
+              setLoading(false);
+            })
+          } else {
+            setAlert(results.statusText);
+            setLoading(false);
+          }
         })
         .catch((error) => {
           //TODO: MAKE ERROR MESSAGE HERE
@@ -48,33 +61,35 @@ export default function Items(props) {
     <React.Fragment>
       <Grid item xs={12}>
         <Paper className={classes.paper}>
-          <Title>Items</Title>
-          {loading ? <LinearProgress /> :
-            <Table size="small">
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Owner</TableCell>
-                  <TableCell>Quality</TableCell>
-                  <TableCell>Transfer</TableCell>
-                  <TableCell align="right"></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {items.map(item => (
-                  <TableItem
-                    user={props.user}
-                    org={props.org}
-                    key={item._id}
-                    item={item}
-                    setUpdate={setUpdate}
-                    setUpdateId={setUpdateId}
-                    setLoading={setLoading}
-                  />
-                ))}
-              </TableBody>
-            </Table>}
+          {alert !== '' ? <Alert severity="error">{alert}</Alert> 
+          : <div><Title>Items</Title>
+            {loading ? <LinearProgress /> :
+              <Table size="small">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Owner</TableCell>
+                    <TableCell>Quality</TableCell>
+                    <TableCell>Transfer</TableCell>
+                    <TableCell align="right"></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {items.map(item => (
+                    <TableItem
+                      user={props.user}
+                      org={props.org}
+                      key={item._id}
+                      item={item}
+                      setUpdate={setUpdate}
+                      setUpdateId={setUpdateId}
+                      setLoading={setLoading}
+                    />
+                  ))}
+                </TableBody>
+              </Table>}</div>
+            }
         </Paper>
       </Grid>
 
