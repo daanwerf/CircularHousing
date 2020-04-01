49175f38b801a0ab5e3cc1b2bd78a1ca502cad4f
daanwerf <daanvanderwerf@hotmail.com>
2020-04-01 16:00:29 +0200

    update participant spec tests to be up-to-date with the new participant chaincode

--- packages/participant-cc/tests/participant.spec.ts
+++ packages/participant-cc/tests/participant.spec.ts
@@ -12,8 +12,10 @@ import { Participant, ParticipantController } from '../src';
 
 describe('Participant', () => {
   chai.use(chaiAsPromised);
-  // A fake certificate to emulate multiple wallets
-  const fakeSecondParticipantCert = '-----BEGIN CERTIFICATE-----' +
+  let adapter: MockControllerAdapter;
+  let participantCtrl: ConvectorControllerClient<ParticipantController>;
+  const mockIdentity = 'B6:0B:37:7C:DF:D2:7A:08:0B:98:BF:52:A4:2C:DC:4E:CC:70:91:E1';
+  const mockCertificate = '-----BEGIN CERTIFICATE-----' +
     'MIICjzCCAjWgAwIBAgIUITsRsw5SIJ+33SKwM4j1Dl4cDXQwCgYIKoZIzj0EAwIw' +
     'czELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNh' +
     'biBGcmFuY2lzY28xGTAXBgNVBAoTEG9yZzEuZXhhbXBsZS5jb20xHDAaBgNVBAMT' +
@@ -29,9 +31,23 @@ describe('Participant', () => {
     'CCqGSM49BAMCA0gAMEUCIQCNsmDjOXF/NvciSZebfk2hfSr/v5CqRD7pIHCq3lIR' +
     'lwIgPC/qGM1yeVinfN0z7M68l8rWn4M4CVR2DtKMpk3G9k9=' +
     '-----END CERTIFICATE-----';
-  let adapter: MockControllerAdapter;
-  let participantCtrl: ConvectorControllerClient<ParticipantController>;
-  const mockIdentity = 'B6:0B:37:7C:DF:D2:7A:08:0B:98:BF:52:A4:2C:DC:4E:CC:70:91:E1';
+
+    const mockCertificate2 = '-----BEGIN CERTIFICATE-----' +
+    'NIICjzCCAjWgAwIBAgIUITsRsw5SIJ+33SKwM4j1Dl4cDXQwCgYIKoZIzj0EAwIw' +
+    'czELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNh' +
+    'biBGcmFuY2lzY28xGTAXBgNVBAoTEG9yZzEuZXhhbXBsZS5jb20xHDAaBgNVBAMT' +
+    'E2NhLm9yZzEuZXhhbXBsZS5jb20wHhcNMTgwODEzMDEyOTAwWhcNMTkwODEzMDEz' +
+    'NDAwWjBCMTAwDQYDVQQLEwZjbGllbnQwCwYDVQQLEwRvcmcxMBIGA1UECxMLZGVw' +
+    'YXJ0bWVudDExDjAMBgNVBAMTBXVzZXIzMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcD' +
+    'QgAEcrfc0HHq5LG1UbyPSRLNjIQKqYoNY7/zPFC3UTJi3TTaIEqgVL6DF/8JIKuj' +
+    'IT/lwkuemafacXj8pdPw3Zyqs6OB1zCB1DAOBgNVHQ8BAf8EBAMCB4AwDAYDVR0T' +
+    'AQH/BAIwADAdBgNVHQ4EFgQUHFUlW/XJC7VcJe5pLFkz+xlMNpowKwYDVR0jBCQw' +
+    'IoAgQ3hSDt2ktmSXZrQ6AY0EK2UHhXMx8Yq6O7XiA+X6vS4waAYIKgMEBQYHCAEE' +
+    'XHsiYXR0cnMiOnsiaGYuQWZmaWxpYXRpb24iOiJvcmcxLmRlcGFydG1lbnQxIiwi' +
+    'aGYuRW5yb2xsbWVudElEIjoidXNlcjMiLCJoZi5UeXBlIjoiY2xpZW50In19MAoG' +
+    'CCqGSM49BAMCA0gAMEUCIQCNsmDjOXF/NvciSZebfk2hfSr/v5CqRD7pIHCq3lIR' +
+    'lwIgPC/qGM1yeVinfN0z7M68l8rWn4M4CVR2DtKMpk3G9k9=' +
+    '-----END CERTIFICATE-----';
   
   before(async () => {
     // Mocks the blockchain execution environment
@@ -48,8 +64,11 @@ describe('Participant', () => {
   });
   
   it('should create a participant', async () => {
-    const id = 'my_username';
-    await participantCtrl.register(id, 'Test Participant');
+    const id = 'mockID';
+    const name = 'mockName';
+    const msp = 'mockOrganisation';
+
+    await participantCtrl.register(id, name, msp, mockCertificate);
   
     const justSavedModel = await adapter.getById<Participant>(id);
   
@@ -57,23 +76,30 @@ describe('Participant', () => {
   });
 
   it('should not be able to create a second participant for the same user', async () => {
-    const id = 'my_second_username';
-    await expect(participantCtrl.register(id, 'Test Participant2')).to.be.eventually
+    const id = 'mockID2';
+    const name = 'mockName2';
+    const msp = 'mockOrganisation';
+
+    await expect(participantCtrl.register(id, name, msp, mockCertificate)).to.be.eventually
       .rejectedWith(Error);
   });
 
   it('should not be able to create a participant with the same id', async () => {
     // Fake another certificate
-    (adapter.stub as any).usercert = fakeSecondParticipantCert;
+    (adapter.stub as any).usercert = mockCertificate2;
 
-    const id = 'my_username';
-    await expect(participantCtrl.register(id, 'Test Another Participant')).to.be.eventually
+    const id = 'mockID';
+    const name = 'mockName2';
+    const msp = 'mockOrganisation';
+    await expect(participantCtrl.register(id, name, msp, mockCertificate2)).to.be.eventually
       .rejectedWith(Error);
   });
 
   it('should be able to create a participant for the new user', async () => {
-    const id = 'second_username';
-    await participantCtrl.register(id, 'User 2');
+    const id = 'mockID2';
+    const name = 'mockName2';
+    const msp = 'mockOrganisation';
+    await participantCtrl.register(id, name, msp, mockCertificate2);
   
     const justSavedModel = await adapter.getById<Participant>(id);
   
@@ -81,7 +107,7 @@ describe('Participant', () => {
   });
 
   it('should return a participant', async () => {
-    const id = 'my_username';
+    const id = 'mockID';
     await expect(participantCtrl.get(id)).to.not.be.eventually.rejectedWith(Error);
   });
 
@@ -91,7 +117,7 @@ describe('Participant', () => {
   });
 
   it('should not be able to change identity', async () => {
-    const id = 'my_username';
+    const id = 'mockID';
     const fake_cert = 'FakeCertificate';
     await expect(participantCtrl.changeIdentity(id, fake_cert)).to.be.eventually.rejectedWith(Error);
   });
