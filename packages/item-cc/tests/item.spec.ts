bd95d61851495aa92878afdc41cd200e52df7a52
daanwerf <daanvanderwerf@hotmail.com>
2020-04-01 14:42:27 +0200

    removed end-to-end tests and updated spec tests file to be up-to-date with new Item code

--- packages/item-cc/tests/item.spec.ts
+++ packages/item-cc/tests/item.spec.ts
@@ -1,39 +1,55 @@
 // tslint:disable:no-unused-expression
 import { join } from 'path';
 import { expect } from 'chai';
-import * as uuid from 'uuid/v4';
 import { MockControllerAdapter } from '@worldsibu/convector-adapter-mock';
 import { ClientFactory, ConvectorControllerClient } from '@worldsibu/convector-core';
 import 'mocha';
 
 import { Item, ItemController } from '../src';
+import { Participant, ParticipantController } from 'participant-cc';
 
 describe('Item', () => {
+  const mockCertificate = 'B6:0B:37:7C:DF:D2:7A:08:0B:98:BF:52:A4:2C:DC:4E:CC:70:91:E1';
+  let owner = new Participant({
+    id: "mockID",
+    name: "mockName",
+    msp: "mockOrganisation",
+    certificate: mockCertificate
+  })
   let adapter: MockControllerAdapter;
   let itemCtrl: ConvectorControllerClient<ItemController>;
+  let participantCtrl: ConvectorControllerClient<ParticipantController>;
   
   before(async () => {
     // Mocks the blockchain execution environment
     adapter = new MockControllerAdapter();
     itemCtrl = ClientFactory(ItemController, adapter);
+    participantCtrl = ClientFactory(ParticipantController, adapter);
 
     await adapter.init([
       {
         version: '*',
         controller: 'ItemController',
         name: join(__dirname, '..')
+      },
+      {
+        version: '*',
+        controller: 'ParticipantController',
+        name: join(__dirname, '../../participant-cc')
       }
     ]);
 
     adapter.addUser('Test');
   });
   
-  it('should create a default model', async () => {
+  it('should initialize an Item', async () => {
+    await participantCtrl.$withUser('Test').create(owner);
+
     const modelSample = new Item({
-      id: uuid(),
-      name: 'Test',
-      created: Date.now(),
-      modified: Date.now()
+      name: "item1",
+      ownerID: owner.id,
+      quality: "Good",
+      materials: "mockMaterial1, mockMaterial2"
     });
 
     await itemCtrl.$withUser('Test').create(modelSample);
