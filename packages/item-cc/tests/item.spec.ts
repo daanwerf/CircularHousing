15af3908b2261a7212b235a2789d6a25a501ae67
daanwerf <daanvanderwerf@hotmail.com>
2020-04-06 16:04:34 +0200

    bugfix for previous commit

--- packages/item-cc/tests/item.spec.ts
+++ packages/item-cc/tests/item.spec.ts
@@ -105,7 +105,7 @@ describe('Item', () => {
       const itemID = await foundItem[0].id;
       const retrievedItem = await adapter.getById<Item>(itemID);
   
-      expect(retrievedItem.itemHistory[0].type).to.be.a('CreateEvent');
+      expect(retrievedItem.itemHistory[0].type).to.be.eventually.eql('CREATE');
     });
 
   // Test for create item
