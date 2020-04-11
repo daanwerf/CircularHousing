3963d8f526cdaa33ada7d70b7a939f4ca2257a28
daanwerf <daanvanderwerf@hotmail.com>
2020-04-11 16:27:36 +0200

    fix typo

--- packages/item-cc/tests/item.spec.ts
+++ packages/item-cc/tests/item.spec.ts
@@ -198,7 +198,7 @@ describe('Item', () => {
     });
     const itemID = await foundItem[0].id;
     console.log(foundItem[0].itemOwner)
-    expect(itemCtrl.withUser('NotOwner').updateName(itemID, "item1NewName").catch(e => e.responses[0].error.message)).to.be.eventually.eql(`You are not allowed to do this action, only mockName is allowed to`);
+    expect(itemCtrl.$withUser('NotOwner').updateName(itemID, "item1NewName").catch(e => e.responses[0].error.message)).to.be.eventually.eql(`You are not allowed to do this action, only mockName is allowed to`);
   });
 
   // Test for update quality
