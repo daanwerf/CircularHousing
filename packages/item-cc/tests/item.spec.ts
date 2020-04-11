83f8844017fb7e8d0f2a436a62a7fb243813c225
daanwerf <daanvanderwerf@hotmail.com>
2020-04-11 14:57:18 +0200

    update other tests to

--- packages/item-cc/tests/item.spec.ts
+++ packages/item-cc/tests/item.spec.ts
@@ -123,14 +123,16 @@ describe('Item', () => {
   // Test for create item
   it('should throw an error, as the mocked user is not the owner of the item', async () => {
     // Simulate being the user with an fingerprint that doesnt exist
-    // adapter.stub['fingerprint'] = mockIdentity;
+    adapter.stub['fingerprint'] = mockIdentity;
+    (adapter.stub as any).usercert = mockCertificate;
 
     const itemName = "item1";
-    const ownerID = "mockID";
+    const ownerID = "mockID2";
     const itemQuality = "Good";
     const materials = "mockMaterial1, mockMaterial2";
 
     expect(itemCtrl.create(itemName, ownerID, itemQuality, materials).catch(e => e.responses[0].error.message)).to.be.eventually.eql('You are not allowed to do this action, only mockName is allowed to');
+    (adapter.stub as any).usercert = mockAdmincertificate;
   });
 
   // Test for create item
