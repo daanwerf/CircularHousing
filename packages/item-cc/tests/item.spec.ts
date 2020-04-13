3522df315e9847af7f3ff735a24fa5df666d3eb7
Tim W <wissel.tim@gmail.com>
2020-04-13 19:53:15 +0200

    Remove commented test

--- packages/item-cc/tests/item.spec.ts
+++ packages/item-cc/tests/item.spec.ts
@@ -362,24 +362,6 @@ describe('Item', () => {
     expect(itemCtrl.proposeTransfer(itemID, "mockID").catch(e => e.responses[0].error.message)).to.be.eventually.eql('Given item does not currently exist on the ledger');
   });
 
-  // Test for answer transfer item
-  // it('should throw an error, as this is not the participant the proposal was made to', async () => {
-  //   // First, successfully propose an item to participant mockName2, as mockName
-  //   adapter.stub['fingerprint'] = mockIdentity;
-  //   const foundItem = await Item.query(Item, {
-  //     'selector': {
-  //       'name': "item1NewName",
-  //     }
-  //   });
-  //   const itemID = await foundItem[0].id;
-  //
-  //   const newOwner = "mockID2";
-  //   await itemCtrl.proposeTransfer(itemID, newOwner);
-  //
-  //   // Try to answer proposal as the wrong user
-  //   expect(itemCtrl.$withUser('NotOwner').answerProposal(itemID, true).catch(e => e.responses[0].error.message)).to.be.eventually.eql(`You are not allowed to do this action, only mockName2 is allowed to`);
-  // });
-
   // Test for update quality item
   it('should throw an error, as this is not the owner of the item', async () => {
     const foundItem = await Item.query(Item, {
